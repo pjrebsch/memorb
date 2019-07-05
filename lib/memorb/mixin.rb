e1536ddb384f76b19dd70c900a5d6844fda43735
Patrick Rebsch <pjrebsch@gmail.com>
2019-07-05 01:04:59 -0400

    Reorganize code in Mixin

--- lib/memorb/mixin.rb
+++ lib/memorb/mixin.rb
@@ -1,15 +1,48 @@
 module Memorb
   module Mixin
 
-    module ClassMethods
+    module IntegrationClassMethods
       def inherited(child)
         Mixin.mixin(child)
       end
     end
 
-    class << self
+    module MixinClassMethods
+      def prepended(base)
+        @base_name = base.name
+      end
 
-      @@mixins = Store.new
+      def name
+        "Memorb(#{ @base_name })"
+      end
+
+      alias_method :inspect, :name
+
+      def register(name)
+        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def #{ name }(*args, &block)
+            memorb.fetch(:"#{ name }", *args, block) do
+              super
+            end
+          end
+        RUBY
+      end
+
+      def unregister(name)
+        begin
+          remove_method(name)
+        rescue NameError
+          # If attempting to unregister a method that isn't currently
+          # registered, Ruby will raise an exception. Simply catching
+          # it here makes the process of registering and unregistering
+          # thread-safe.
+        end
+      end
+    end
+
+    @@mixins = Store.new
+
+    class << self
 
       def mixin(base)
         @@mixins.fetch(base) { mixin! base }
@@ -23,45 +56,14 @@ module Memorb
 
       def mixin!(base)
         new.tap do |m|
-          base.extend ClassMethods
+          base.extend IntegrationClassMethods
           base.prepend m
         end
       end
 
       def new
         Module.new do
-          class << self
-            def prepended(base)
-              @base_name = base.name
-            end
-
-            def name
-              "Memorb(#{ @base_name })"
-            end
-
-            alias_method :inspect, :name
-
-            def register(name)
-              class_eval <<-RUBY, __FILE__, __LINE__ + 1
-                def #{ name }(*args, &block)
-                  memorb.fetch(:"#{ name }", *args, block) do
-                    super
-                  end
-                end
-              RUBY
-            end
-
-            def unregister(name)
-              begin
-                remove_method(name)
-              rescue NameError
-                # If attempting to unregister a method that isn't currently
-                # registered, Ruby will raise an exception. Simply catching
-                # it here makes the process of registering and unregistering
-                # thread-safe.
-              end
-            end
-          end
+          extend MixinClassMethods
 
           def initialize(*)
             @memorb_cache = Memorb::Cache.new(integration: self.class)
