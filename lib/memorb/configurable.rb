077861e6d0e29b683ef7e703a5194921d1f0c3c5
Patrick Rebsch <pjrebsch@gmail.com>
2019-07-05 02:18:02 -0400

    Clean up Configurable

--- lib/memorb/configurable.rb
+++ lib/memorb/configurable.rb
@@ -1,17 +1,15 @@
 module Memorb
   class Configurable < Module
 
-    def included(base)
-      super
-      @integration = base
-      mixin = Mixin.mixin(base)
-      @methods.each { |name| mixin.register(name) }
-    end
-
     def initialize(*args)
-      @integration = nil
       @args = args
       @methods = args
+    end
+
+    def included(base)
+      super
+      mixin = Mixin.mixin(base)
+      @methods.each { |name| mixin.register(name) }
     end
 
     def inspect
