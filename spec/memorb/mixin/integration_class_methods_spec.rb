dd6a559719333af5d796684f27bdc283cc646f3b
Patrick Rebsch <pjrebsch@gmail.com>
2019-07-18 19:22:45 -0400

    Update some terminology

--- spec/memorb/mixin/integration_class_methods_spec.rb
+++ spec/memorb/mixin/integration_class_methods_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Memorb::Mixin::IntegrationClassMethods do
   let(:integration) { Class.new(Counter) { include Memorb } }
 
   describe '::inherited' do
-    it 'makes child classes of integration classes get their own mixin' do
+    it 'makes children of integrations get their own mixin' do
       child_integration = Class.new(integration)
       mixin = Memorb::Mixin.for(child_integration)
       expect(mixin).not_to be(nil)
@@ -11,7 +11,7 @@ RSpec.describe Memorb::Mixin::IntegrationClassMethods do
     end
   end
   describe '::memorb' do
-    it 'returns the mixin for the integrating class' do
+    it 'returns the mixin for the integration' do
       mixin = Memorb::Mixin.for(integration)
       expect(integration.memorb).to be(mixin)
     end
