673553244ef025114d3dea2133cffeab61cff08d
Patrick Rebsch <pjrebsch@gmail.com>
2019-10-12 14:28:51 -0400

    Randomize spec execution order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@
 
 require_relative '../lib/memorb'
 
-seed = ::RSpec.configuration.seed || ::Random.new_seed
-puts "Seed used for tests: #{ seed }"
-srand(seed)
-
 module SpecHelper
   class << self
 
